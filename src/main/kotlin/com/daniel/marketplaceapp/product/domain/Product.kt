381715a59a194fe28863da0495ced08f7cb9177a
Thunder22020 <d.shchipanov@innopolis.university>
2026-05-17 12:13:14 +0500

    Refactor: add product domain layer

--- src/main/kotlin/com/daniel/marketplaceapp/product/domain/Product.kt
+++ src/main/kotlin/com/daniel/marketplaceapp/product/domain/Product.kt
@@ -1,17 +1,48 @@
 package com.daniel.marketplaceapp.product.domain
 
 import com.daniel.marketplaceapp.core.domain.Money
+import com.daniel.marketplaceapp.product.dto.UpdateProductRequest
 import com.daniel.marketplaceapp.product.enums.ProductStatus
+import com.daniel.marketplaceapp.product.exception.ProductAlreadyDeletedException
 import java.time.Instant
 import java.util.UUID
 
 class Product(
     var id: UUID?,
-    val title: String,
+    var title: String,
     var description: String?,
     var price: Money,
     var sellerId: UUID,
     var status: ProductStatus,
     var createdAt: Instant,
     var updatedAt: Instant?,
-)
+) {
+    fun deleteProduct() {
+        status = ProductStatus.DELETED
+        setUpdatedAt()
+    }
+
+    fun updateStatus(newStatus: ProductStatus) {
+        checkProductNotDeletedOrThrow()
+        status = newStatus
+        setUpdatedAt()
+    }
+
+    fun update(req: UpdateProductRequest) {
+        checkProductNotDeletedOrThrow()
+        req.title?.let { title = it }
+        req.description?.let { description = it }
+        req.price?.let { price = Money(it) }
+        setUpdatedAt()
+    }
+
+    private fun checkProductNotDeletedOrThrow() {
+        if (status == ProductStatus.DELETED) {
+            throw ProductAlreadyDeletedException("Product $id has been deleted")
+        }
+    }
+
+    private fun setUpdatedAt() {
+        updatedAt = Instant.now()
+    }
+}
