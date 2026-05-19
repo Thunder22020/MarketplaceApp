e1bb438f7cbf8cdd2b04be7144a6ab86eaaba7e0
Thunder22020 <d.shchipanov@innopolis.university>
2026-05-19 12:58:36 +0500

    Refactor: optimize imports

--- src/main/kotlin/com/daniel/marketplaceapp/product/controller/ProductController.kt
+++ src/main/kotlin/com/daniel/marketplaceapp/product/controller/ProductController.kt
@@ -7,10 +7,19 @@ import com.daniel.marketplaceapp.product.dto.UpdateProductRequest
 import com.daniel.marketplaceapp.product.mapper.toResponse
 import com.daniel.marketplaceapp.product.service.ProductService
 import jakarta.validation.Valid
+import java.util.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/products")
